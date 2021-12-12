b80bc0ea4fbcca344c8534474215ba5b995aed46
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-12-11 22:16:09 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import requests
 import threading
 
+
 def submit_form():
     while True:
         data = {}
         r = requests.post(
             url=
-            "https://docs.google.com/forms/d/e/form-id/formResponse",
+            "https://docs.google.com/forms/d/e/form-id/formResponse",  # change form-id to the random string of characters in the form, check README.md for more details
             data=str(data))
         print(r.status_code)
+
 
 threads = []
 
